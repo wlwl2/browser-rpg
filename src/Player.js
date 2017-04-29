02ee215a0df74efb739ab3236e44d1086c0e0513
William <manvinedi@gmail.com>
2017-04-29 13:25:40 +0800

    made drawImage comments neater

--- src/Player.js
+++ src/Player.js
@@ -9,15 +9,45 @@ Player.prototype.draw = function draw (ctx) {
   var img = new Image()
   img.src = 'src/sprites/characters.png'
   ctx.drawImage(
+
+    // An element to draw into the context.
     img,
-    9, // The X coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
-    42, // The Y coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
-    this.size, // The width of the sub-rectangle of the source image to draw into the destination context. If not specified, the entire rectangle from the coordinates specified by sx and sy to the bottom-right corner of the image is used.
-    this.size, // The height of the sub-rectangle of the source image to draw into the destination context.
-    this.x, // The X coordinate in the destination canvas at which to place the top-left corner of the source image.
-    this.y, // The Y coordinate in the destination canvas at which to place the top-left corner of the source image.
-    this.size, // The width to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in width when drawn.
-    this.size // The height to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in height when drawn.
+
+    // The X coordinate of the top left corner of the sub-rectangle of the
+    // source image to draw into the destination context.
+    9,
+
+    // The Y coordinate of the top left corner of the sub-rectangle of the
+    // source image to draw into the destination context.
+    42,
+
+    // The width of the sub-rectangle of the source image to draw into the
+    // destination context. If not specified, the entire rectangle from the
+    // coordinates specified by sx and sy to the bottom-right
+    // corner of the image is used.
+    this.size,
+
+    // The height of the sub-rectangle of the source image to draw into
+    // the destination context.
+    this.size,
+
+    // The X coordinate in the destination canvas at which to place the
+    // top-left corner of the source image.
+    this.x,
+
+    // The Y coordinate in the destination canvas at which to place the
+    // top-left corner of the source image.
+    this.y,
+
+    // The width to draw the image in the destination canvas. This allows
+    // scaling of the drawn image. If not specified, the image is not
+    // scaled in width when drawn.
+    this.size,
+
+    // The height to draw the image in the destination canvas. This allows
+    // scaling of the drawn image. If not specified, the image is not
+    // scaled in height when drawn.
+    this.size
   )
 }
 
--- version-history.md
+++ version-history.md
@@ -1,9 +1,14 @@
-
-
-Version 0.0.2 29 Apr 2017
-
+Todo:
 collision detection
 independent monster patrols
 player moves depending on controller
 maze?
 level editor
+
+Version 0.0.2 29 Apr 2017
+Scene initialization
+Border initialization
+
+Version 0.0.1 8 Apr 2017
+
+Started Game Development
