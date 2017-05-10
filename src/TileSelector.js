46c5771c4254abea60a71e9cf3015c81de62105f
William Lim <manvinedi@gmail.com>
2017-05-11 01:56:41 +0800

    tile selector mouse info

--- src/TileSelector.js
+++ src/TileSelector.js
@@ -1,17 +1,37 @@
 export default function TileSelector () {
   const tiles = document.querySelector('.tile-list').children
+  const currentTile = document.querySelector('.currently-selected-tile')
+  const resetTileButton = document.querySelector('.reset-tile-button')
+  const mouseInfo = document.querySelector('.mouse-info')
+  function removeCurrentlySelectedTile () {
+    if (currentTile.childNodes[1]) {
+      currentTile.removeChild(currentTile.childNodes[1])
+    }
+    if (mouseInfo.childNodes[1]) {
+      mouseInfo.removeChild(mouseInfo.childNodes[1])
+    }
+  }
+  if (tiles.length === 0) return
   for (var i = 0; i < tiles.length; i++) {
     tiles[i].addEventListener('click', function (event) {
-      const sprite = document.createElement('div')
-      sprite.setAttribute('draggable', 'true')
+      const sprite1 = document.createElement('div')
+      sprite1.setAttribute('draggable', 'true')
       const dataEntityNumber = event.target.getAttribute('data-entity-number')
-      sprite.setAttribute('data-entity-number', dataEntityNumber)
-      sprite.className = event.target.className
-      const currentTile = document.querySelector('.currently-selected-tile')
-      if (currentTile.childNodes[1]) {
-        currentTile.removeChild(currentTile.childNodes[1])
-      }
-      currentTile.appendChild(sprite)
+      sprite1.setAttribute('data-entity-number', dataEntityNumber)
+      sprite1.className = event.target.className
+      const sprite2 = sprite1.cloneNode(true)
+      removeCurrentlySelectedTile()
+      currentTile.appendChild(sprite1)
+      mouseInfo.appendChild(sprite2)
     }, false)
   }
+  if (!resetTileButton) return
+  resetTileButton.addEventListener('click', function (event) {
+    removeCurrentlySelectedTile()
+  }, false)
+
+  document.addEventListener('mousemove', function mouseInfoPosition (event) {
+    mouseInfo.style.left = String(event.pageX + 20) + 'px'
+    mouseInfo.style.top = String(event.pageY + 20) + 'px'
+  }, false)
 }
