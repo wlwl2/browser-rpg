a54b6f4633aa549453a78b854183ad0290d2817c
William Lim <manvinedi@gmail.com>
2017-05-12 23:50:44 +0800

    removed standard js errors on Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -1,4 +1,4 @@
-var img = new Image()
+var img = document.createElement('img')
 img.src = '../sprites/characters.png'
 
 function Sprite (img, width, height, positions) {
@@ -7,24 +7,23 @@ function Sprite (img, width, height, positions) {
   this.height = height
   this.positions = positions
 }
-
-Sprite.prototype = {
-  draw: function (x, y, position) {
-    var pos = this.positions[position]
-
-    ctx.drawImage(
-        this.img,
-        pos[0], // The X coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
-        pos[1], // The Y coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
-        this.width, // The width of the sub-rectangle of the source image to draw into the destination context. If not specified, the entire rectangle from the coordinates specified by sx and sy to the bottom-right corner of the image is used.
-        this.height, // The height of the sub-rectangle of the source image to draw into the destination context.
-        x, // The X coordinate in the destination canvas at which to place the top-left corner of the source image.
-        y, // The Y coordinate in the destination canvas at which to place the top-left corner of the source image.
-        this.width, // The width to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in width when drawn.
-        this.height // The height to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in height when drawn.
-      )
-  }
-}
+// Sprite.prototype = {
+//   draw: function (x, y, position) {
+//     var pos = this.positions[position]
+//
+//     ctx.drawImage(
+//       this.img,
+//       pos[0], // The X coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+//       pos[1], // The Y coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+//       this.width, // The width of the sub-rectangle of the source image to draw into the destination context. If not specified, the entire rectangle from the coordinates specified by sx and sy to the bottom-right corner of the image is used.
+//       this.height, // The height of the sub-rectangle of the source image to draw into the destination context.
+//       x, // The X coordinate in the destination canvas at which to place the top-left corner of the source image.
+//       y, // The Y coordinate in the destination canvas at which to place the top-left corner of the source image.
+//       this.width, // The width to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in width when drawn.
+//       this.height // The height to draw the image in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in height when drawn.
+//     )
+//   }
+// }
 
 var monster = new Sprite(img, 30, 30, [[8, 114]])
 monster.draw(140, 200, 0)
