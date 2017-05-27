6d2d96831c225b9b52ee1fb57f5265d5bf63ac0a
William <manvinedi@gmail.com>
2017-05-27 12:41:36 +0800

    asset loader version 1 complete

--- src/index.js
+++ src/index.js
@@ -45,13 +45,21 @@ TileSelector()
 
 // Waits for all the images etc. to load, then runs the step function.
 function assetLoader () {
-  // const images = document.querySelectorAll(".")
-  // for (var i = 0; i < array.length; i++) {
-  //   array[i]
-  // }
-  window.addEventListener('load', function (event) {
-    step()
-  }, false)
+  const images = document.querySelectorAll('img')
+  if (images.length === 0) return
+  const imageListLength = images.length
+  // console.log(imageListLength)
+  let loadedImagesCounter = 0
+  for (var i = 0; i < imageListLength; i++) {
+    images[i].addEventListener('load', function (event) {
+      loadedImagesCounter += 1
+      // console.log(loadedImagesCounter)
+      if (imageListLength === loadedImagesCounter + 1) {
+        // console.log('all done')
+        step()
+      }
+    }, false)
+  }
 }
 assetLoader()
 
