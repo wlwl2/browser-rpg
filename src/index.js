f8eb8746c61cb26055b764e3772b7dd4d74cb9db
William Lim <manvinedi@gmail.com>
2017-05-28 22:51:07 +0800

    window load check seems to be better than checking if images are loaded

--- src/index.js
+++ src/index.js
@@ -42,24 +42,27 @@ const world = new World()
 TileSelector()
 
 // Waits for all the images etc. to load, then runs the step function.
-function assetLoader () {
-  const images = document.querySelectorAll('img')
-  if (images.length === 0) return
-  const imageListLength = images.length
-  // console.log(imageListLength)
-  let loadedImagesCounter = 0
-  for (var i = 0; i < imageListLength; i++) {
-    images[i].addEventListener('load', function (event) {
-      loadedImagesCounter += 1
-      // console.log(loadedImagesCounter)
-      if (imageListLength === (loadedImagesCounter + 1)) {
-        // console.log('all done')
-        step()
-      }
-    }, false)
-  }
-}
-assetLoader()
+// function assetLoader () {
+//   const images = document.querySelectorAll('img')
+//   if (images.length === 0) return
+//   const imageListLength = images.length
+//   // console.log(imageListLength)
+//   let loadedImagesCounter = 0
+//   for (var i = 0; i < imageListLength; i++) {
+//     images[i].addEventListener('load', function (event) {
+//       loadedImagesCounter += 1
+//       // console.log(loadedImagesCounter)
+//       if (imageListLength === (loadedImagesCounter + 1)) {
+//         // console.log('all done')
+//         step()
+//       }
+//     }, false)
+//   }
+// }
+window.addEventListener('load', function (event) {
+  // assetLoader()
+  step()
+}, false)
 
 // Refactor.
 var map = {}
