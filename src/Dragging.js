f40fad0d26b577c567ebc65995c78709a8c559c7
William Lim <manvinedi@gmail.com>
2017-06-03 19:01:29 +0800

    dragging update

--- src/Dragging.js
+++ src/Dragging.js
@@ -1,7 +1,11 @@
 export default function dragging () {
+  // On mousemove, the positions of the tiles that were hovered during the
+  // mouse hold will be added to the hoveredOverTiles array, then on mouseup,
+  // the canvas is redrawn with the tiles in that array. :)
   const canvasElement = document.querySelector('canvas')
   if (!canvasElement) return
   let mouseHeld = false
+  let hoveredOverTiles = []
 
   canvasElement.addEventListener('mousedown', function (event) {
     mouseHeld = true
