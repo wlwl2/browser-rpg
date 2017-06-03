d0d228cf27526075e33187f99b65c45be194b503
William Lim <manvinedi@gmail.com>
2017-06-04 03:25:20 +0800

    added sass support

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+// module.exports = function (grunt) {
+//   /* Do not prepend ./ to the paths- it will prevent newly added files
+//   to not be detected during grunt watch */
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     sass: {
+//       dist: {
+//         files: [{
+//           expand: true,
+//           cwd: 'src/sass',
+//           src: ['*.scss'],
+//           dest: 'src/styles',
+//           ext: '.css',
+//           extDot: 'last'
+//         }]
+//       }
+//     },
+//     watch: {
+//       sass: {
+//         files: ['./src/sass/*.scss'],
+//         tasks: ['sass']
+//       }
+//     }
+//   })
+//
+//   // Load the plugins
+//   grunt.loadNpmTasks('grunt-contrib-sass')
+//   grunt.loadNpmTasks('grunt-contrib-watch')
+//
+//   // Default task(s).
+//   grunt.registerTask('default', ['watch'])
+// }
