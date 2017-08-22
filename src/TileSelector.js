b90cb09b8035ce82a3e307b3eae8605d1666d58d
William Lim <manvinedi@gmail.com>
2017-08-22 16:02:15 +0800

    updated tile selector

--- src/TileSelector.js
+++ src/TileSelector.js
@@ -6,7 +6,7 @@ export default function TileSelector (canvas) {
   const mouseInfo = document.querySelector('.mouse-info')
 
   // Resets currently selected tile.
-  function removeCurrentlySelectedTile () {
+  function removeCurrentlySelectedTile (canvas) {
     if (currentTile.childNodes[1]) {
       currentTile.removeChild(currentTile.childNodes[1])
     }
@@ -16,35 +16,41 @@ export default function TileSelector (canvas) {
     if (mouseInfo.childNodes[1]) {
       mouseInfo.removeChild(mouseInfo.childNodes[1])
     }
+    if (canvas.getAttribute('data-cursor')) {
+      canvas.removeAttribute('data-cursor')
+    }
   }
-
   // When each of the tiles are clicked, update the tile selected.
-  if (tiles.length === 0) return
-  for (var i = 0; i < tiles.length; i++) {
-    tiles[i].addEventListener('click', function (event) {
-      const sprite1 = document.createElement('div')
-      sprite1.setAttribute('draggable', 'true')
-      const dataEntityNumber = event.target.getAttribute('data-entity-number')
-      sprite1.setAttribute('data-entity-number', dataEntityNumber)
-      sprite1.className = event.target.className
-      const sprite2 = sprite1.cloneNode(true)
-      removeCurrentlySelectedTile()
-      currentTile.appendChild(sprite1)
-      mouseInfo.appendChild(sprite2)
-    }, false)
+  function tileSelect (canvas) {
+    if (tiles.length === 0) return
+    for (var i = 0; i < tiles.length; i++) {
+      tiles[i].addEventListener('click', function (event) {
+        const sprite1 = document.createElement('div')
+        sprite1.setAttribute('draggable', 'true')
+        const dataEntityNumber = event.target.getAttribute('data-entity-number')
+        sprite1.setAttribute('data-entity-number', dataEntityNumber)
+        sprite1.className = event.target.className
+        const sprite2 = sprite1.cloneNode(true)
+        removeCurrentlySelectedTile(canvas)
+        currentTile.appendChild(sprite1)
+        mouseInfo.appendChild(sprite2)
+        canvas.setAttribute('data-cursor', 'pointer')
+      }, false)
+    }
   }
+  tileSelect(canvas)
 
   // On reset button click, reset the currently selected tile.
   if (resetTileButton) {
     resetTileButton.addEventListener('click', function (event) {
-      removeCurrentlySelectedTile()
+      removeCurrentlySelectedTile(canvas)
     }, false)
   }
 
   // When the esc key is pressed, clear the currently selected tile.
   window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
-      removeCurrentlySelectedTile()
+      removeCurrentlySelectedTile(canvas)
     }
   }, false)
 
