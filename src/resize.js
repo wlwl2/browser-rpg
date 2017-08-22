7fe2173d7aebf2dbf00bd6cbd12662e89bdcf911
William Lim <manvinedi@gmail.com>
2017-08-22 12:49:48 +0800

    Revert "update"
    This reverts commit 11a614e25d156cbca06f0d7acfa621c1be0ab424.

--- src/resize.js
+++ src/resize.js
@@ -1,21 +1,21 @@
 export default function resize () {
-  // var height = document.body.getBoundingClientRect().height
-  // var width = document.body.getBoundingClientRect().width
-  //
-  // if (width > height) {
-  //   document.querySelector('canvas').setAttribute('data-horizontal', 'yes')
-  // } else {
-  //   document.querySelector('canvas').setAttribute('data-horizontal', 'no')
-  // }
-  //
-  // window.addEventListener('resize', function (event) {
-  //   var height = document.body.getBoundingClientRect().height
-  //   var width = document.body.getBoundingClientRect().width
-  //
-  //   if (width > height) {
-  //     document.querySelector('canvas').setAttribute('data-horizontal', 'yes')
-  //   } else {
-  //     document.querySelector('canvas').setAttribute('data-horizontal', 'no')
-  //   }
-  // }, false)
+  var height = document.body.getBoundingClientRect().height
+  var width = document.body.getBoundingClientRect().width
+
+  if (width > height) {
+    document.querySelector('canvas').setAttribute('data-horizontal', 'yes')
+  } else {
+    document.querySelector('canvas').setAttribute('data-horizontal', 'no')
+  }
+
+  window.addEventListener('resize', function (event) {
+    var height = document.body.getBoundingClientRect().height
+    var width = document.body.getBoundingClientRect().width
+
+    if (width > height) {
+      document.querySelector('canvas').setAttribute('data-horizontal', 'yes')
+    } else {
+      document.querySelector('canvas').setAttribute('data-horizontal', 'no')
+    }
+  }, false)
 }
