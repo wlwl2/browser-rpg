03cc474d1546db9c00a764d73acfaaaac0ab9116
William Lim <manvinedi@gmail.com>
2017-09-14 03:32:13 +0800

    update

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,14 @@
 export default function menu () {
   var menu = document.querySelector('.menu')
+  var overlay = document.querySelector('.overlay')
   window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
       if (menu.getAttribute('data-hidden') === 'no') {
         menu.setAttribute('data-hidden', 'yes')
+        overlay.setAttribute('data-hidden', 'yes')
       } else {
         menu.setAttribute('data-hidden', 'no')
+        overlay.setAttribute('data-hidden', 'no')
       }
     }
   }, false)
