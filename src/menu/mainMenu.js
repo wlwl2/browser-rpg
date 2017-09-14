16f199d6740e0f4f037dd036c6e7104b5b1db79d
William Lim <manvinedi@gmail.com>
2017-09-14 09:06:51 +0800

    update

--- src/menu/mainMenu.js
+++ src/menu/mainMenu.js
@@ -1,4 +1,8 @@
 export default function mainMenu () {
+  const menu = document.querySelector('.menu')
+  const overlay = document.querySelector('.overlay')
+  const mouseInfo = document.querySelector('.mouse-info')
+
   const startMenuItems = document.querySelectorAll('.start-menu__menu li')
 
   // Arrow key events.
@@ -18,21 +22,26 @@ export default function mainMenu () {
         selectedMenuItem.previousElementSibling.className = 'selected'
       }
     }
+
+    if (event.key === 'Enter') {
+      const selectedMenuItem = document.querySelector('.start-menu__menu li.selected')
+      if (selectedMenuItem.textContent === 'Continue') {
+        menu.setAttribute('data-hidden', 'yes')
+        overlay.setAttribute('data-hidden', 'yes')
+        mouseInfo.setAttribute('style', 'display: block;')
+      }
+    }
   }, false)
 
-
   // Escape key events.
-  const menu = document.querySelector('.menu')
-  const overlay = document.querySelector('.overlay')
-  const mouseInfo = document.querySelector('.mouse-info')
   window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
-      // If the menu is shown.
+      // If the menu is shown, then hide the menu.
       if (menu.getAttribute('data-hidden') === 'no') {
         menu.setAttribute('data-hidden', 'yes')
         overlay.setAttribute('data-hidden', 'yes')
         mouseInfo.setAttribute('style', 'display: block;')
-      } else { // If the menu is hidden.
+      } else { // If the menu is hidden, show the menu.
         menu.setAttribute('data-hidden', 'no')
         overlay.setAttribute('data-hidden', 'no')
         mouseInfo.setAttribute('style', 'display: none;')
